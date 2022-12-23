14983ae112c4665c26a80818f479192690928928
Razvy Cretu <razvan.cretu97@gmail.com>
2022-12-23 23:29:29 +0200

    add-to-cart button that uses redux store

--- components/Layout.js
+++ components/Layout.js
@@ -1,21 +1,59 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
 import { AppBar, Box, Button, IconButton } from "@mui/material";
-import { useTheme } from "@mui/material/styles";
+import { styled, useTheme } from "@mui/material/styles";
 import Container from "@mui/material/Container";
 import Brightness4Icon from "@mui/icons-material/Brightness4";
 import Brightness7Icon from "@mui/icons-material/Brightness7";
+import ShoppingBasketRoundedIcon from "@mui/icons-material/ShoppingBasketRounded";
 // import { useThemeCtx } from "../contexts/themeContext";
+import { selectCart } from "../store/cartSlice";
+import { useDispatch, useSelector } from "react-redux";
 
 const pages = [
   { name: "Home", path: "/" },
   { name: "Store", path: "/store" },
 ];
 
+const Links = styled(Box)(({ theme }) => ({
+  height: "5vh",
+  display: "flex",
+  alignItems: "center",
+  marginLeft: "1rem",
+  "& :nth-of-type(n)": {
+    marginLeft: ".5rem",
+  },
+}));
+
+const CartIcon = styled(IconButton)(({ theme, items }) => ({
+  position: "relative",
+  "&:after": {
+    content: `"${items.length.toString()}"`,
+    display: items.length === 0 ? "none" : "flex",
+    height: "28px",
+    width: "28px",
+    alignItems: "center",
+    justifyContent: "center",
+    background: "red",
+    borderRadius: "50%",
+    position: "absolute",
+    top: 0,
+    right: 0,
+    padding: "15px",
+    transform: "scale(.5) translate(60%,-60%)",
+    fontSize: "1.4rem",
+    fontWeight: 700,
+    border: "3px solid white",
+  },
+}));
+
 const Layout = (props) => {
   const theme = useTheme();
   // const toggleTheme = useThemeCtx();
   const router = useRouter();
+
+  const cart = useSelector(selectCart);
+  // console.log(cart);
 
   return (
     <>
@@ -28,18 +66,11 @@ const Layout = (props) => {
               <Brightness4Icon />
             )}
           </IconButton>
+          <CartIcon items={cart}>
+            <ShoppingBasketRoundedIcon />
+          </CartIcon>
         </Box>
-        <Box
-          sx={{
-            height: "5vh",
-            display: "flex",
-            alignItems: "center",
-            marginLeft: "1rem",
-            "& :nth-of-type(n)": {
-              marginLeft: ".5rem",
-            },
-          }}
-        >
+        <Links>
           {pages.map((page) => {
             return (
               <Button
@@ -54,7 +85,7 @@ const Layout = (props) => {
               </Button>
             );
           })}
-        </Box>
+        </Links>
       </AppBar>
       <Container component="main">{props.children}</Container>
     </>
