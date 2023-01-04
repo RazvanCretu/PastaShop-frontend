8470337a88d861f5a413e3196cb64a44245dec26
Razvy Cretu <razvan.cretu97@gmail.com>
2023-01-04 14:12:39 +0200

    Update on Confirmation Component

--- components/Checkout/Confirmation.js
+++ components/Checkout/Confirmation.js
@@ -1,4 +1,4 @@
-import { Typography, Divider } from "@mui/material";
+import { Box, Typography, Divider } from "@mui/material";
 import { selectCart } from "../../store/cartSlice";
 import { useSelector } from "react-redux";
 
@@ -10,16 +10,27 @@ const Confirmation = ({ values }) => {
       <Typography variant="h4">Shipping Information</Typography>
       <Divider variant="middle" sx={{ mt: 1, mb: 1 }} />
       {Object.keys(values).map((item, i) => (
-        <Typography key={i} variant="subtitle1">
-          {JSON.stringify(values[item])}
-        </Typography>
+        <Box key={i} display="flex" justifyContent="space-around" gap="1rem">
+          <Typography variant="subtitle1" color="secondary">
+            {item}
+          </Typography>
+          <Typography variant="subtitle1">{values[item]}</Typography>
+        </Box>
       ))}
-      <Typography variant="h4">Cart Items</Typography>
+      <Typography variant="h4">Products</Typography>
       <Divider variant="middle" sx={{ mt: 1, mb: 1 }} />
       {Object.values(cartItems).map((item, i) => (
-        <Typography key={i} variant="subtitle1">
-          {JSON.stringify(item)}
-        </Typography>
+        <Box
+          key={i}
+          display="flex"
+          justifyContent="space-between"
+          m="auto 5rem"
+        >
+          <Typography variant="h6">{item.name}</Typography>
+          <Box>
+            <Typography variant="body1">{`${item.qty} x ${item.price} $`}</Typography>
+          </Box>
+        </Box>
       ))}
     </>
   );
